ba8d32ef05fd81bba4afc980645c180ed03c620d
billycp <billycp@mail.uc.edu>
2023-11-16 23:46:55 -0500

    Add new class for serial port

---
+++ Rocket_TM_BSC/Model/SerialPort_TMData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Rocket_TM_BSC.Model
+{
+    public class SerialPort_TMData
+    {
+
+        public SerialPort_TMData()
+        {
+            
+        }
+
+        OpenNewPort(string COM, int Baud, Parity parity, int dataBits, StopBits stopBits)
+        {
+            _serialport = new SerialPort();
+        }
+    }
+}
